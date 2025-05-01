7d4d1b8a51deb58e286ea85d43e69c0056882420
c8899701316 <c8866701316@gmail.com>
2025-05-01 12:08:44 +0530

    -- chatbot implement

--- src/App.js
+++ src/App.js
@@ -36,6 +36,8 @@ import Sidebar from './components/Sidebar';
 import Keyconfiguration from './components/keyconfiguration';
 import Tracking from './components/Tracking';
 import Users from './components/Users';
+import Chatbot from './components/Chatbot';
+
 
 const App = () => {
 
@@ -45,15 +47,13 @@ const App = () => {
   return (
 
     <div className='d-flex flex-column'>
-      <Router>
+      {/* <Router>
         {token && <Sidebar setToken={setToken} />}
         {
           token ?
-            <Routes>
+          <Routes>
               <>
-                {/* <Route path="/login" element={<Login setRole={setRole} setToken={setToken} role={role} />} /> */}
                 <Route path="/dashboard/client" element={<ClientPage role={role} />} />
-                {/* <Route index element={<Navigate to="page1" role={role} />} /> Redirect to Page1 by default */}
                 <Route path="/dashboard/Stores" element={<Page1 role={role} />} />
                 <Route path="/dashboard/page2" element={<Page2 role={role} />} />
                 <Route path="/dashboard/promt" element={<Promptpage role={role} />} />
@@ -61,11 +61,36 @@ const App = () => {
                 <Route path="/dashboard/tracking" element={<Tracking role={role} />} />
                 <Route path="/dashboard/users" element={<Users role={role} />} />
                 {/* <Route path="*" element={<Navigate to="/login" />} /> */}
-              </>
+
+              {/* </>
             </Routes> :
                 <Login setRole={setRole} setToken={setToken} role={role} />
-            //  <Navigate to={"/login"} />
-        }
+
+              }
+              <Chatbot position="bottom-right" height={500} width={400} />
+      </Router>  */}
+      <Router>
+        {token && <Sidebar setToken={setToken} />}
+        {token ? (
+          <>
+            <Routes>
+              <Route path="/dashboard/client" element={<ClientPage role={role} />} />
+              <Route path="/dashboard/Stores" element={<Page1 role={role} />} />
+              <Route path="/dashboard/page2" element={<Page2 role={role} />} />
+              <Route path="/dashboard/promt" element={<Promptpage role={role} />} />
+              <Route path="/dashboard/keyconfiguration" element={<Keyconfiguration role={role} />} />
+              <Route path="/dashboard/tracking" element={<Tracking role={role} />} />
+              <Route path="/dashboard/users" element={<Users role={role} />} />
+              {/* <Route path="*" element={<Navigate to="/dashboard/client" />} /> */}
+            </Routes>
+            <Chatbot position="bottom-right" height={500} width={400} />
+          </>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login setRole={setRole} setToken={setToken} role={role} />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
